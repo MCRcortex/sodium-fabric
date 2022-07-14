bace7055feb165cf7cbf3a81b77b573ce9db9cba
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2022-07-15 02:46:03 +1000

    sparse buf

---
+++ src/main/java/net/caffeinemc/sodium/render/buffer/arena/sparse/SparseBufferArena.java
@@ -0,0 +1,8 @@
+package net.caffeinemc.sodium.render.buffer.arena.sparse;
+
+public class SparseBufferArena {
+    private static class Allocation {
+        long offset;
+        long length;
+    }
+}
