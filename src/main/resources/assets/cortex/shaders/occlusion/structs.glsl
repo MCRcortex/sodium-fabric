82b794b7a3eafdb6ced72e40545c4e27a1d502f9
jellysquid3 <jellysquid3@users.noreply.github.com>
2022-12-21 17:09:35 +1000

    more structs

--- src/main/resources/assets/cortex/shaders/occlusion/structs.glsl
+++ src/main/resources/assets/cortex/shaders/occlusion/structs.glsl
@@ -5,14 +5,12 @@
 //uint baseOffset
 
 
+struct Region {//32 bytes
+    ivec4 id_start_count_x;//Count can be a uint16 or even possibly a uint8
+    ivec4 y_z_sizexz_sizey_meta;//Size can be a half float
+};
 
-struct Section {//64 bytes
-    ivec4 offsetAndId;
+struct Section {//128 bytes //64 bytes should be possible
 
 };
 
-
-struct Region {//64 bytes
-    ivec4 offsetAndId;
-    ivec4 aabbAnd;
-};
