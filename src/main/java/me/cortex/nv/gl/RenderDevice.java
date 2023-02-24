b8eb2bb45ca610c9301288dc0ea4ca7c4f87ccfb
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2023-02-24 13:58:46 +1000

    Continued work on upload buffer, added size limit to segment manager

--- src/main/java/me/cortex/nv/gl/RenderDevice.java
+++ src/main/java/me/cortex/nv/gl/RenderDevice.java
@@ -1,4 +1,9 @@
 package me.cortex.nv.gl;
 
+import me.cortex.nv.gl.buffers.PersistentMappedBuffer;
+
 public class RenderDevice {
+    public PersistentMappedBuffer createClientMappedBuffer(long size) {
+        return null;
+    }
 }
